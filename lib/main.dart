15c016866c6ab0a7a9ef86c5865296aad8bb1458
rex-suresh <99480758+rex-suresh@users.noreply.github.com>
2023-10-11 09:11:03 +0530

    Add Home page rough

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:musify_reborn/src/screens/HomeScreen.dart';
 
-void main() {
-  runApp(const Text(
-    'Hello mom!',
-    textDirection: TextDirection.ltr,
-  ));
+class MyApp extends StatelessWidget {
+  static ThemeData theme = ThemeData.dark(useMaterial3: true);
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const HomeScreen(),
+    );
+  }
 }
+
+void main() => runApp(const MyApp());
