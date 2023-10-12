d93f6bcd4489926263d5fd5cef86d76bafb635e4
rex-suresh <99480758+rex-suresh@users.noreply.github.com>
2023-10-12 19:29:45 +0530

    Realised tabbar cannot be seperated 🥲

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:musify_reborn/src/app/main_screen.dart';
-import 'package:musify_reborn/src/screens/player_screen.dart';
-import 'package:musify_reborn/src/screens/search_screen.dart';
 
-class MyApp extends StatelessWidget {
+class Musify extends StatelessWidget {
   static ThemeData theme = ThemeData.dark(useMaterial3: true);
-  const MyApp({super.key});
+  const Musify({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/': (context) => const MainScreen(),
-        '/search': (context) => const SearchScreen(),
-        '/player': (context) => const PlayerScreen(),
-      },
       theme: theme,
       home: const MainScreen(),
     );
   }
 }
+
+void main() => runApp(const Musify());
--- lib/src/app/main_screen.dart
+++ lib/src/app/main_screen.dart
@@ -1,21 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:musify_reborn/src/app/nav_bar.dart';
+import 'package:musify_reborn/src/misc/icon.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentIndex = 0;
+
+  final BottomNavigationBarItem homeNav = const BottomNavigationBarItem(
+    label: 'Home',
+    icon: IconOf(icon: Icons.home_rounded),
+  );
+
+  final BottomNavigationBarItem searchNav = const BottomNavigationBarItem(
+    label: 'Search',
+    icon: IconOf(icon: Icons.search_rounded),
+  );
+
+  final BottomNavigationBarItem playerNav = const BottomNavigationBarItem(
+    label: 'Player',
+    icon: IconOf(icon: Icons.music_note_rounded),
+  );
+
+  void _onItemTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: const NavBar(),
-      body: SafeArea(
-          child: Container(
-        padding: const EdgeInsets.all(20),
-        child: const Text(
-          "Hey mom!",
-          textDirection: TextDirection.ltr,
-        ),
-      )),
-    );
+        body:
+            Text('This hurts! $currentIndex', textDirection: TextDirection.ltr),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [homeNav, searchNav, playerNav],
+          enableFeedback: true,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.white,
+          onTap: _onItemTapped,
+          currentIndex: currentIndex,
+        ));
   }
 }
+
+// SafeArea(
+//           child: Container(
+//         padding: const EdgeInsets.all(20),
+//         child: const Text(
+//           "Hey mom!",
+//           textDirection: TextDirection.ltr,
+//         ),
